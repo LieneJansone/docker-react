af795278d053cb99ab2cdc04b30aee56897f36b9
root <root@ip-172-31-25-157.eu-west-1.compute.internal>
2020-02-15 18:29:22 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="App-title">Welcome </h1>
+           
+<p className="App-intro">
+          Bye there :)
         </p>
         <a
           className="App-link"
